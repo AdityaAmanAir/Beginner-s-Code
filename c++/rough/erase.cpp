0bbc38232b614c8b14af7b3f0101c2cc76056cb6
AdityaAmanAir <amanaditya1947@proton.me>
2025-03-17 23:18:48 +0530

    update 03.17v

--- c++/rough/erase.cpp
+++ c++/rough/erase.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 int main() {
-    int age;
-    cout << "Enter your age: "; // Prompt user
-    cin >> age;                 // Read input into variable
-    cout << "You are " << age << " years old." << endl
-
-    cerr;// Error mess
-    clog << "Log: Program executed." << endl;       // Log message
+    int t,a;
+    cin>>t;
+    while(t--){
+        cin>>a;
+        cout<<(a>=30)? "YES\n":"NO\n";
+    }
     return 0;
 }
