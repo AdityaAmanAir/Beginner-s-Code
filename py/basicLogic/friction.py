deb43d77714a8d8b45488f69a3abeb0c292718f1
AdityaAmanAir <amanaditya1947@proton.me>
2025-03-21 23:16:27 +0530

    update 03.21v

---
+++ py/basicLogic/friction.py
@@ -0,0 +1,7 @@
+coF=0.45
+g=9.81
+s=100
+a=coF*g
+u=(2*a*s)**(1/2)
+
+print(f"The speed of car was {u:.3f} m/s when the Driver applied the breaks")
---
+++ py/basicLogic/workDone.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+mass =2
+g=9.81
+height =100
+work_Done=mass*g*height
+print(f'Work Done by the object {work_Done}')
+
