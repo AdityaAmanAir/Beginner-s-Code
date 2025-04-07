1e0d21fe07d670d2bd0bf3e5d9cdc5b987e42768
AdityaAmanAir <amanaditya1947@proton.me>
2025-04-07 23:55:41 +0530

    update 04.07v

--- py/rough/erase2.py
+++ py/rough/erase2.py
@@ -1,4 +1,28 @@
-a=[2,4,2,3,55,1,0,77]
-c=a.copy()
-b=a.sort()
-print(a[-1])
+def coinf(c,l,r):
+    first=c[l] 
+    second=c[l+1]
+    x=0
+    cf=0
+    cs=0
+    for i in range(len(c)+1):
+        x+=1
+        if x>=l and x<=r:
+            if c[i]==first:
+                cf+=1
+            if c[i]==second:
+                cs+=1    
+    cl=r-l+1-cf-cs
+    print(cl+cf)
+
+coin=input()
+c=[]
+for i in coin:
+    c.append(i)
+t=int(input())
+for i in range(t):
+    l,r=map(int, input().split())
+    coinf(c,l,r)
+    
+    
+
+
