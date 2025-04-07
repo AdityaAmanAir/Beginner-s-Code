d0e328e7de1e171d07df9c91f4372a6bdd8d3038
AdityaAmanAir <amanaditya1947@proton.me>
2025-04-07 23:55:41 +0530

    update 04.07v

--- c++/rough/erase.cpp
+++ c++/rough/erase.cpp
@@ -3,46 +3,16 @@
 
 using namespace std;
 
-// Function to count valid sequences
-int count_valid_sequences(vector<int>& A, int k) {
-    int n = A.size();
-    int count = 0;
-
-    for (int i = 0; i < n; i++) {
-        int current_sum = 0;
-        for (int j = i; j < n; j++) {
-            current_sum += A[j]; 
-            if (current_sum >= k) {
-                count++;
-                break; // Stop early if sum >= k
-            }
-        }
-    }
-    return count;
-}
-
-int main() {
+int main(){
     int t;
-    cin >> t;
-    
-    while (t--) {
-        int n, k, x;
-        cin >> n >> k >> x;
-        
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a;
+        b=2*a;
+        while(b--){
+            
         }
-
-        // Extend list `a` by repeating `k` times
-        vector<int> b;
-        for (int i = 0; i < k; i++) {
-            b.insert(b.end(), a.begin(), a.end());
-        }
-
-        // Print the count of valid sequences
-        cout << count_valid_sequences(b, x) << endl;
     }
-
     return 0;
 }
