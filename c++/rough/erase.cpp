e1afaa99bb6abe56a1d92262022c7fbcfd6896a5
AdityaAmanAir <amanaditya1947@proton.me>
2025-04-08 23:49:31 +0530

    update 04.08

--- c++/rough/erase.cpp
+++ c++/rough/erase.cpp
@@ -1,18 +1,14 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     int t;
     cin>>t;
     while(t--){
-        int a,b;
-        cin>>a;
-        b=2*a;
-        while(b--){
-            
-        }
+        int a,b,i,count=0,c=0;
+        cin>>a>>b;
+        while
+        
     }
     return 0;
 }
--- py/rough/erase.py
+++ py/rough/erase.py
@@ -1,25 +1,23 @@
-a=int(input())
-while(a>0):
-    a-=1
-    b=int(input())
-    c=2*b
-    x=[]
-    while(c>0):
-        c-=1
-        m=int(input())
-        x.append(m)
-    x.sort()
-    j=0
-    i=1
-    while i <len(x)-1:
-            if x[i]-x[i-1]==x[i+1]-x[i]:
-                print("No")
-                j=1
-                break
-            i+=1
-        
-    if(j==0) :
-        print("YES")      
+import math
 
+def is_prime(n):
+    if n< 2:
+        return False
+    if n% 2 == 0:
+        return n == 2  
+    r = int(math.sqrt(n))
+    for i in range(3, r+1, 2):
+        if n % i == 0:
+            return False
+    return True
 
-    
+t = int(input())
+for j in range(t):
+    a, b = map(int, input().split())
+    if b==2 and a==1:
+        print("YES")
+    else:    
+        if b == 1 and is_prime(a):
+            print("YES")
+        else:
+            print("NO")
