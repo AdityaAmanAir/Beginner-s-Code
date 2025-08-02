152921d55bdc4d90ea92bb965a25d2c5a9189cec
AdityaAmanAir <amanaditya1947@proton.me>
2025-08-02 14:55:12 +0530

    update 8.2

---
+++ c++/basicLogic/ASCIIvalues.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i=-1; i<=280; i++){
+        cout<<i<<" = "<<char(i)<<"\n";
+    }
+    // cout<<int("\n");
+
+
+    return 0;
+}
--- c++/rough/erase.cpp
+++ c++/rough/erase.cpp
@@ -1,11 +1,51 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-   int arr[]={10,20,30,40,50};
-int*p = arr;
-cout<< *(p+3)<<endl;
-cout<< *(p+1)<<endl;
-cout<< *(p)<<endl;
+   int t;
+   cin>>t;
+   while(t--){
+    int a;
+    cin>>a;
+    int sum=0,check=0,zero=0;
+    bool z=false;
+    while(a--){
+        int x;
+        cin>>x;
+        if(x==0){
+            z=true;
+            zero++;
+            check++;
+        }
+        if(z==true && x<3){
+            if(x==check && a!=0){
+                check++;
+            }else if(x<check && a!=0){
+                sum+=1;
+                z=false;
+            }else if(x==check && a==0){
+                check++;
+                sum+=(x*(x+1)/2);
+            }else if(x<check && a==0){
+                sum+=1;
+                sum+=(x*(x+1)/2);  
+                z=false;
+            }else if(x>check ){
+                sum+=check;
+                z=false;
+            }
+        }
+        if(z==false || x>=4){
+            sum+=x;
+        
+        }
+    }
+    if(zero>1){
+        zero=zero-1;
+    }else{
+        zero=0;
+    }
+    cout<<sum-zero<<"\n";
+   }
 return 0;
 }
