1b8e314b1d731d55c92fe9fab23b6caa79b16576
AdityaAmanAir <amanaditya1947@proton.me>
2025-08-06 13:10:49 +0530

    update 8.6v

--- c++/rough/erase.cpp
+++ c++/rough/erase.cpp
@@ -1,51 +1,9 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
 int main() {
-   int t;
-   cin>>t;
-   while(t--){
-    int a;
-    cin>>a;
-    int sum=0,check=0,zero=0;
-    bool z=false;
-    while(a--){
-        int x;
-        cin>>x;
-        if(x==0){
-            z=true;
-            zero++;
-            check++;
-        }
-        if(z==true && x<3){
-            if(x==check && a!=0){
-                check++;
-            }else if(x<check && a!=0){
-                sum+=1;
-                z=false;
-            }else if(x==check && a==0){
-                check++;
-                sum+=(x*(x+1)/2);
-            }else if(x<check && a==0){
-                sum+=1;
-                sum+=(x*(x+1)/2);  
-                z=false;
-            }else if(x>check ){
-                sum+=check;
-                z=false;
-            }
-        }
-        if(z==false || x>=4){
-            sum+=x;
-        
-        }
-    }
-    if(zero>1){
-        zero=zero-1;
-    }else{
-        zero=0;
-    }
-    cout<<sum-zero<<"\n";
-   }
+   long long int t,a,b;
+   cin>>t>>b>>a;
+
+   cout<<((t/a)*(b/a))+((b%a==0)?0:(t/a))+((t%a==0)?0:(b/a))+(((b%a!=0)&&(t%a!=0))?1:0);
 return 0;
 }
