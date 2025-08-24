e61f729b570e23a00b10f3864f2aa13ccaee1a3d
AdityaAmanAir <amanaditya1947@proton.me>
2025-08-25 01:37:57 +0530

    Update 8.13V

--- c++/rough/erase.cpp
+++ c++/rough/erase.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
-int main() {
-   long long int t,a,b;
-   cin>>t>>b>>a;
-
-   cout<<((t/a)*(b/a))+((b%a==0)?0:(t/a))+((t%a==0)?0:(b/a))+(((b%a!=0)&&(t%a!=0))?1:0);
-return 0;
+int main(){
+    string a,b;
+    getline(cin, a);
+    getline(cin, b);
+    bool did_not_break = true;
+    int i=a.length();
+    for (int j=0; j<i ; j++){
+        if(a[j]>96){
+            a[j]-=32;
+        }
+        if(b[j]>96){
+            b[j]-=32;
+        }
+        if(a[j]>b[j]){
+            cout<<1;
+            did_not_break = false;
+            break;
+        }if(a[j]<b[j]){
+            cout<<-1;
+            did_not_break = false;
+            break;
+        }
+    }if(did_not_break){cout<<0;}
+    return 0;
 }
